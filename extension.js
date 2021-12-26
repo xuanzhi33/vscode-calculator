3e2fad1b18a10e75bfe3aa82c874c95197c2332b
xuanzhi33 <xuanzhi33@qq.com>
2021-12-26 12:01:22 +0800

    update functions

--- extension.js
+++ extension.js
@@ -10,17 +10,10 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('calculator.calculator', function () {
-		// The code you place here will be executed every time your command is executed
 
 		// Get the text that user has selected
 		let editor = vscode.window.activeTextEditor;
-		
-		
 		
 
 		// Ask user to input a math expression
