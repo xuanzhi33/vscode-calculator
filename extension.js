b73c1f549c3c78a1118f337a47aa4d7d1c647a15
xuanzhi33 <xuanzhi33@qq.com>
2021-12-26 11:55:17 +0800

    first commit

---
+++ extension.js
@@ -0,0 +1,53 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('calculator.calculator', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Get the text that user has selected
+		let editor = vscode.window.activeTextEditor;
+		
+		
+		
+
+		// Ask user to input a math expression
+		vscode.window.showInputBox({
+			placeHolder: 'Please input a math expression',
+			ignoreFocusOut: true,
+			value: editor.document.getText(editor.selection)
+		}).then(function (input) {
+			if (input) {
+				// calculate the result
+				let result = eval(input);
+				// show the result
+				vscode.window.showInformationMessage("The result is "+result.toString());
+			}
+		}
+		);
+
+
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
